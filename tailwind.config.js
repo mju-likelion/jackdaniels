9efcd924aa0521b2ccb66acca05902995bc35b21
ndaemy <yuyaebean@gmail.com>
2023-02-18 23:46:34 +0900

    setting: tailwind 테마에 프로젝트 컬러 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#1C1B1A',
+        gray1: '#2F2F2F',
+        gray2: '#AAAAAA',
+        gray3: '#5F5F5F',
+        orange: '#FF872D',
+        blue1: '#547AFF',
+        blue2: '#10319D',
+        red: '#FF6868',
+        green: '#6AFFB8',
+      },
+    },
   },
   plugins: [],
 };
