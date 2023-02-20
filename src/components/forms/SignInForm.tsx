ef069d2ede9b3f0d27fccbcf25d7d2bf34f7f9a7
ndaemy <yuyaebean@gmail.com>
2023-02-21 02:19:19 +0900

    feat: 로그인 form validate 로직 추가

--- src/components/forms/SignInForm.tsx
+++ src/components/forms/SignInForm.tsx
@@ -1,23 +1,39 @@
 import { FC } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import isEmail from 'validator/lib/isEmail';
+
+type FormValues = {
+  email: string;
+  password: string;
+};
 
 export const SignInForm: FC = () => {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = data => console.log(data);
+
   return (
-    <form className="flex flex-col w-72 xs:w-full xs:m-8">
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex flex-col w-72 xs:w-full xs:m-8"
+    >
       <input
         type="email"
-        name="email"
         autoComplete="email"
         required
         placeholder="이메일"
         className="bg-gray1 rounded-none rounded-t-md p-2.5 pl-3.5 outline-0 box-border border-gray3 border-b"
+        {...register('email', {
+          required: true,
+          validate: email => isEmail(email),
+        })}
       />
       <input
         type="password"
-        name="password"
         autoComplete="current-password"
         required
         placeholder="비밀번호"
         className="bg-gray1 rounded-none rounded-b-md p-2.5 pl-3.5 outline-0 box-border"
+        {...register('password', { required: true })}
       />
       <button
         type="submit"
