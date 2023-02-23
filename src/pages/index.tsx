40f926fb517c11efd815620c77ee59cf7a1c2312
ndaemy <yuyaebean@gmail.com>
2023-02-24 04:55:36 +0900

    feat: NavBar 디자인 tailwind ui 에서 가져오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { NavBar } from '@/components/common/NavBar';
+
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <NavBar />;
 }
