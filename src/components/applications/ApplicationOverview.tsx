f1997062e22d08ab3de27a9328269813c81a4e56
ndaemy <yuyaebean@gmail.com>
2023-03-07 03:53:14 +0900

    feat: 지원서 페이지 그나마 이쁘게..

--- src/components/applications/ApplicationOverview.tsx
+++ src/components/applications/ApplicationOverview.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { IData } from '@/types/ApplicationsType';
+import { format } from 'date-fns';
 
 interface IProps {
   info: IData;
@@ -11,10 +12,12 @@ export const ApplicationOverview = ({ info, onClick }: IProps): JSX.Element => {
     <div className='flex h-14 w-full items-center border-b border-gray3 p-3 text-sm'>
       <div className='basis-2/12 text-base'>{info.name}</div>
       <div className='basis-3/12 text-gray2'>{info.major}</div>
-      <div className='basis-2/12 text-gray2'>{info.grade}</div>
+      <div className='basis-2/12 text-gray2'>{info.grade}학년</div>
       <div className='basis-2/12 text-gray2'>{info.part}</div>
       <div className='flex basis-3/12 justify-between'>
-        <div className='text-gray2'>{info.createdDate}</div>
+        <div className='text-gray2'>
+          {format(new Date(info.createdDate), 'yyyy.MM.dd hh:mm:ss')}
+        </div>
         <button className='text-blue1' onClick={onClick}>
           detail
         </button>
